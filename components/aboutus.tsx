e6a7e18a1c105a95bf2cf7242e812da1272e8396
Agon Ademaj <agonademaj@aol.com>
2022-07-19 18:46:50 +0200

    Make some changes

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -5,7 +5,7 @@ const AboutUs: React.FC = () => {
   return (
     <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
       <div className='container mx-auto px-11'>
-        <p className='leading-tight max-w-5xl mx-auto lg:text-4xl text-3xl tracking-tight'>
+        <p className='leading-tight max-w-5xl mx-auto lg:text-4xl text-2xl tracking-tight'>
           <strong>Hi there, I&apos;m Agon Ademaj</strong>, I work as frontend
           backend developer creating thoughtful experiences. I always strive to
           achieve the highest standard possible, at any given task in any
@@ -22,7 +22,7 @@ const AboutUs: React.FC = () => {
       <div className='container px-11 text-center mt-28 mx-auto'>
         <h2 className='font-bold'>What I do</h2>
         <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5 lg:gap-20'>
-          <Member image='cubes' name='Product Design' />
+          <Member image='product-design' name='Product Design' />
           <Member image='system' name='Design Systems' />
           <Member image='coding' name='Development' />
           <Member image='prototyping' name='Prototyping' />
